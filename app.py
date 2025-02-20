d5e5f1296f5d4a05e1f8f0a76031036e1d781a62
Ricardo Torres <150391159+Rick6dev@users.noreply.github.com>
2025-02-19 22:18:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def home():
     return jsonify({"message": "Hola Mundo!", "status": "ok"})
